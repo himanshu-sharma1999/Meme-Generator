16e8eeea981a36a6bafe12476318517652518f1c
himanshu-sharma1999 <hvsharma1999@gmail.com>
2025-04-14 16:27:17 +0530

    Initial commit - Meme Genrator

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+export default function App() {
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
+}
+
+                    
+                                                2
+
